babb91c247d34e8d684f1bb04d17ce5f1879d5f5
htlin2 <htlin2@gmail.com>
2019-02-04 12:59:35 -0800

    initial setup with react front-end

---
+++ client/src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 1,
+      dFirstName: 'd1First',
+      dLastName: 'd1Last',
+      appointments: {},
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        apoweifjw
+      </div>
+    )
+  }
+}
+
+export default App;
